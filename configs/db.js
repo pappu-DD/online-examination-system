5c304f591204b9529c891a998449a3681a88dc39
pappu-DD <codeactionwala@gmail.com>
2025-04-25 20:32:06 +0530

    deployement problem fixing -2

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import postgres from "postgres";
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined in environment variables');
--- configs/schema.js
+++ configs/schema.js
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const feedbacks = pgTable('feedbacks', {
   id: serial('id').primaryKey(),
